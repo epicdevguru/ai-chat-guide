8158e318653114605664b6e733c117afac457ea7
Toshiyuki <toshihara@hotmail.com>
2024-06-07 01:59:34 +0800

    Update main.py based on gpt's recommendation

--- tempScript.py
+++ tempScript.py
@@ -1,3 +1,4 @@
+
 #----------- for deployment
 __import__('pysqlite3')
 import sys
@@ -21,9 +22,11 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 from pathlib import Path
 from openai import OpenAI
+#from langchain.chains import LLMChain
+
 import json
-import streamlit as st
-import base64
+import streamlit as st;
+
 
 st.title("Chat AI agent")
 st.write("---")
@@ -37,6 +40,21 @@ embedding_function = OpenAIEmbeddings()
 documents = [Document(page_content=str(item), metadata={"source": "gikAiJSONFile.json"}) for item in jsonData["dishes"]]
 db = Chroma.from_documents(documents, embedding_function)
 
+# loader = PyPDFLoader("vertopal.com_gikAiJSONFile.pdf")
+# pages = loader.load_and_split()
+
+# text_splitter = RecursiveCharacterTextSplitter(
+#     # Set a really small chunk size, just to show.
+#     chunk_size=300,
+#     chunk_overlap=20,
+#     length_function=len,
+#     is_separator_regex=False,
+# )
+
+# texts = text_splitter.split_documents(pages)
+# embeddings_model = OpenAIEmbeddings()
+# db = Chroma.from_documents(texts, embeddings_model, persist_directory="./chroma.db")
+
 @dataclass
 class Message:
     actor: str
@@ -47,13 +65,13 @@ ASSISTANT = "ai"
 MESSAGES = "messages"
 
 def get_llm() -> ChatOpenAI:
-    return ChatOpenAI(model_name="gpt-4", temperature=0)
+    return ChatOpenAI(model_name="gpt-4o",temperature=0)
 
 def initialize_session_state():
     if MESSAGES not in st.session_state:
         st.session_state[MESSAGES] = [Message(actor=ASSISTANT, payload="Hai! Saya Ika, selamat datang di Galeri Indonesia Kaya. Butuh info tentang Indonesia? Tanya saja! Saya siap membantu.")]
     if "llm_chain" not in st.session_state:
-        st.session_state["llm_chain"] = RetrievalQA.from_chain_type(get_llm(), retriever=db.as_retriever())
+        st.session_state["llm_chain"] = RetrievalQA.from_chain_type(get_llm(), retriever=db.as_retriever())#get_llm_chain()
 
 def get_llm_chain_from_session():
     return st.session_state["llm_chain"]
@@ -68,6 +86,8 @@ prompt: str = st.chat_input("Enter a prompt here")
 
 client = OpenAI()
 
+speech_file_path = Path(__file__).parent / "speech.mp3"
+
 if prompt:
     st.session_state[MESSAGES].append(Message(actor=USER, payload=prompt))
     st.chat_message(USER).write(prompt)
@@ -79,14 +99,24 @@ if prompt:
             model="tts-1",
             voice="alloy",
             input=dataResponse,
-        )
-        with open("output.mp3", "wb") as f:
-            f.write(response.content)
         
+        )
+        response.stream_to_file("output.mp3")
         st.session_state[MESSAGES].append(Message(actor=ASSISTANT, payload=dataResponse))
         st.chat_message(ASSISTANT).write(dataResponse)
+        
 
-        # Embed and play the audio file in Streamlit
-        audio_file = open("output.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mp3")
+
+# question = st.text_input('Ask a question.')#"What is the content of this document?"
+# if st.button('Submit'):
+#     with st.spinner('Wait for it ...'):
+#         llm = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0)
+#         # retriever_from_llm = MultiQueryRetriever.from_llm(
+#         #    retriever=db.as_retriever(), llm=llm
+#         # )
+#         #docs = retriever_from_llm.get_relevant_documents(query=question)
+#         qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+#         result = qa_chain({"query": question})
+#         st.write(result["result"])
+
+
